be486753db131b9a8d3a6bf29d2f967546a92d09
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 18:04:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Artist
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    #binding.pry
+    Song.all.select { |song| song.artist == self }
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+end
